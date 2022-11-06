cc13886ac9be8c02ad81b49bcf5d576375218d45
Léo Marché <marche.leo2@gmail.com>
2022-11-06 22:52:00 +0100

    deploy.sh now is in tha deploy-infrastructure repo

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-echo "## Applying terraform"
-terraform init && terraform apply
-
-echo "## Waiting 1m for cluster creation"
-sleep 1m
-
-echo "## Executing ansible"
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory playbooks/main.yaml
